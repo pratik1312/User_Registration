d90902b61883e11307c7d356e9e65aca25f9ebbe
pratik1312 <patilpratik194@gmail.com>
2020-10-03 22:29:19 +0530

    Added:File validates the first name

--- RegistrationMatch.sh
+++ RegistrationMatch.sh
@@ -1,2 +1,10 @@
 ##!/bin/bash -x
 echo "Welcome to the User Registration Validation code"
+read -p "Enter the First Name : " FirstName
+firstnamepattern="^([A-Z]*[a-z]){3,}$"
+if [[ $FirstName =~ $firstnamepattern ]]
+then
+	echo "First Name is Valid"
+else
+	echo "First Name is Invalid"
+fi
